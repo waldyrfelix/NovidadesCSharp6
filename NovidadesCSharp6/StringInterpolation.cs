162ab2429909a51e2360ec8971bbe2fbbfbe704a
Waldyr Felix <waldyrfelix@gmail.com>
2015-02-05 01:55:13 +0000

    Exemplo de String Interpolation

---
+++ NovidadesCSharp6/StringInterpolation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NovidadesCSharp6
+{
+    [TestClass]
+    public class StringInterpolation
+    {
+        [TestMethod]
+        public void TesteInterpolaçãoSimples()
+        {
+            const string nome = "Maria";
+            var str = $"{nome} comprou pão.";
+
+            Assert.AreEqual("Maria comprou pão.", str);
+        }
+
+
+        [TestMethod]
+        public void TesteInterpolaçãoComFormatação()
+        {
+            var p = new Produto
+            {
+                Nome = "Colírio",
+                Preço = 25.9m,
+                SKU = 123
+            };
+
+            var sentença = $"{p.Nome} ({p.SKU :d5}) - VLR: {p.Preço :n2}";
+            // Colírio (00123) - VLR: 25.90
+
+            Assert.AreEqual("Colírio (00123) - VLR: 25.90", sentença);
+        }
+
+
+        [TestMethod]
+        public void TesteInterpolaçãoComData()
+        {
+            var p = new Produto
+            {
+                Nome = "Mouse"
+            };
+
+            var sentença = $"Meu {{{p.Nome}}} pifou as {DateTime.Now :t}.";
+            // sentença: Meu {Mouse} pifou as 12:34.
+
+            Assert.AreEqual("Meu {Mouse} pifou as " + DateTime.Now.ToShortTimeString() + ".", sentença);
+        }
+    }
+
+    public class Produto
+    {
+        public int SKU { get; set; }
+        public string Nome { get; set; }
+
+        public decimal Preço { get; set; }
+    }
+}
